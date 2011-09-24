53bf435afd586c89f9a531bb16791fe3afc05e5c
Sun, Liwen <sunliwen@gmail.com>
2011-09-24 15:50:09 +0800

    Change 牛奶白's link

--- footer.php
+++ footer.php
@@ -3,7 +3,9 @@
 <div id="footer">
 <!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
 	<p>
-		Powered by <a href="http://wordpress.org/">WordPress</a> | <a href="http://www.azeemazeez.com/blogs/white-as-milk/" title="White as Milk theme for Wordpress">White as Milk</a> | <a href="http://sunliwen.com/white-as-milk-cn" title="牛奶白">牛奶白</a> 
+		Powered by <a href="http://wordpress.org/">WordPress</a>
+                 | <a href="http://www.azeemazeez.com/blogs/white-as-milk/" title="White as Milk theme for Wordpress">White as Milk</a> 
+                 | <a href="http://sunliwen.com/white-as-milk" title="牛奶白">牛奶白</a> 
         <br />订阅<a href="<?php bloginfo('rss2_url'); ?>">文章</a> 或者 <a href="<?php bloginfo('comments_rss2_url'); ?>">评论</a>.
 		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
 	</p>
